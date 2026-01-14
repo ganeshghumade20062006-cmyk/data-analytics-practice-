d933c40f291387ce98f6d2e4fed3bf8f25ad1286
Ganesh Ghumade <ganeshghumade20062006@gmail.com>
2026-01-14 19:18:51 +0530

    First program in python (Practice)
    1.Single line uses
    2.Multiple line use
    3.Using \n for new line
    4.Using single quote inside double quote

---
+++ python_data_analytics_practice.py
@@ -0,0 +1,83 @@
+print("This file is creted for my data analytics journey")
+print("In this journey i will cover all the required things for the data analytics journey")
+print("I have intrest in coding and i work on my skills")
+print("I am from Nagpur city")
+
+# output:
+# This file is creted for my data analytics journey
+# In this journey i will cover all the required things for the data analytics journey
+# I have intrest in coding and i work on my skills
+# I am from Nagpur city
+
+
+
+# multiple line use
+
+print("""In is file is created for my ata analytics journey
+in this joourney i will cover all the required things for the 
+data anaalytics journey i have intrest in coding and i work on my skills
+ """)
+
+# In is file is created for my ata analytics journey
+# in this joourney i will cover all the required things for the
+# data anaalytics journey i have intrest in coding and i work on my skills
+
+
+print("""
+This is multi-line comments.
+You can write many lines here
+Python ignores this when running.
+""")
+
+# This is multi-line comments.
+# You can write many lines here
+# Python ignores this when running.
+
+print("""
+This function prints a welcome message.
+It is used to shoow basic function usages.
+""")
+
+# This function prints a welcome message.
+# It is used to shoow basic function usages.
+
+
+# using \n for new line
+
+message="welcome to python!\nThis is " \
+"line 2 \nThis is line 3."
+print(message)
+
+# welcome to python!
+# This is line 2
+# This is line 3.
+
+print("This is the first line\n\nThis" \
+"line comes after one blank line.")
+
+# This is the first line
+
+# Thisline comes after one blank line.
+
+
+print("1. python\n2. java\n3. c++\n4." \
+"javaScript")
+
+# 1. python
+# 2. java
+# 3. c++
+# 4.javaScript
+
+# using single quote inside double quote
+
+print('It\'s a good day')
+# It's a good day
+
+print('He said \'hello\' to me')
+# He said 'hello' to me
+
+
+print('Python is east to learn, it\'s fun')
+# Python is east to learn, it's fun
+
+# .....
